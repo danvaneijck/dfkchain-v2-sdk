5ad838b577c5f10d486cbbe1e8d9d3ded8921161
Dan <dan.vaneijck@sensys.co.nz>
2023-06-25 22:46:34 +1200

    tests passing

--- src/router.test.ts
+++ src/router.test.ts
@@ -17,8 +17,8 @@ function checkDeadline(deadline: string[] | string): void {
 
 describe('Router', () => {
   const ETHER = JEWEL.onChain(CHAIN_ID)
-  const token0 = new Token(1, '0x0000000000000000000000000000000000000001', 18, 't0')
-  const token1 = new Token(1, '0x0000000000000000000000000000000000000002', 18, 't1')
+  const token0 = new Token(CHAIN_ID, '0x0000000000000000000000000000000000000001', 18, 't0')
+  const token1 = new Token(CHAIN_ID, '0x0000000000000000000000000000000000000002', 18, 't1')
 
   const pair_0_1 = new Pair(
     CurrencyAmount.fromRawAmount(token0, JSBI.BigInt(1000)),
